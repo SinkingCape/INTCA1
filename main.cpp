a871391c33f265da4c5b5214d596c477ebb21304
SinkingCape <dvdguilfoyle@gmail.com>
2024-07-08 16:36:29 +0000

    CA Code 1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <stdio.h>
+
+int main() {
+    // Initialize variables a, b, and c
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    // Add values of a and b, store it in a new variable d
+    int d = a + b;
+    printf("Value of d (a+b); %d\n",d);
+
+    // Subtract variable c from d, store in a new variable e
+    int e = d - c;
+    printf("Value of e (d-c); %d\n",e);
+
+    return 0;
+}
+
